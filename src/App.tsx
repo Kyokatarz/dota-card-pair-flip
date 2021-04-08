2647aeef9324730faa2ef5085f91f7ee348d2506
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2021-04-09 02:42:30 +0300

    add bg images + styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
+import BackgroundImage from './components/BackGroundImg'
 
 import PlayArea from './components/PlayArea'
 
 function App() {
   return (
     <div id='App'>
+      <BackgroundImage />
       <PlayArea />
     </div>
   )
--- src/components/BackGroundImg/index.tsx
+++ src/components/BackGroundImg/index.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
+import './styles.css'
 
 const BackgroundImage = () => {
-  return (
-    <img
-      id='bg-img'
-      src='./images/png-clipart-cartoon-comics-fan-art-others-legendary-creature-comics.png'
-      alt=''
-    />
-  )
+  return <img id='bg-img' src='/assets/background-img/bg-2.png' alt='' />
 }
 
 export default BackgroundImage
