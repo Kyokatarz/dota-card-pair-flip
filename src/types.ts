7592ccfeb76ff95da21bc76593992844ae497b5d
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2021-04-08 23:02:12 +0300

    Add more types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,12 @@
-export type DataType = {
-  frontOfCardUrl: string
-  cardBackUrlArray: { url: string; uniqueId: number }[]
+export type FrontOfCardType = string
+export type BackOfCardType = {
+  url: string
+  uniqueId: number
 }
+
+export type DataType = {
+  frontOfCardUrl: FrontOfCardType
+  cardBackUrlArray: BackOfCardType[]
+}
+
+export type GameStateType = BackOfCardType[]
